58dbd0b46c35855df9cb19f7eaa846fa91f1e851
Prathyusha <tprathyushareddy31@gmail.com>
2025-01-09 10:18:12 +0530

    third commit

--- src/main/java/ex_01_Java_Basics/Lab002_Practice.java
+++ src/main/java/ex_01_Java_Basics/Lab002_Practice.java
@@ -2,6 +2,10 @@ package ex_01_Java_Basics;
 
 public class Lab002_Practice {
     public static void main(String[] args) {
-
+// Single line comment shorcut is ctrl+/
+/**
+ * Multiple line comment
+ */
+        System.out.println("Just for Practice");
     }
 }
