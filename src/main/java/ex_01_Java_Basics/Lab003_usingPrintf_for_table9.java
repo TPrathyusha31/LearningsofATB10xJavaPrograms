e5815c80e2c73f5ba9514fac17683f3bd33838d4
Prathyusha <tprathyushareddy31@gmail.com>
2025-01-19 13:53:30 +0530

    Jan 19th task part1 commit

--- src/main/java/ex_01_Java_Basics/Lab003_usingPrintf_for_table9.java
+++ src/main/java/ex_01_Java_Basics/Lab003_usingPrintf_for_table9.java
@@ -2,13 +2,13 @@ package ex_01_Java_Basics;
 
 public class Lab003_usingPrintf_for_table9 {
     public static void main(String[] args) {
-        int n=9;
+        int n=9,n2,n3;
 
         System.out.printf("%d*1=%d\n",n,n);
-        System.out.printf("%d*2=%d\n",n,n*2);
-        System.out.printf("%d*3=%d\n",n,n*3);
-        System.out.printf("%d*4=%d\n",n,n*4);
-        System.out.printf("%d*5=%d\n",n,n*5);
+        System.out.printf("%d*2=%d\n",n,n2=n*2);
+        System.out.printf("%d*3=%d\n",n,n3=n*3);
+        System.out.printf("%d*%d=%d\n",n,4,n*4);
+        System.out.printf("%d*%d=%d\n",n,5,n*5);
         System.out.printf("%d*6=%d\n",n,n*6);
         System.out.printf("%d*7=%d\n",n,n*7);
         System.out.printf("%d*8=%d\n",n,n*8);
---
+++ src/main/java/ex_01_Java_Basics/Lab004_GradeCalculator_ternaryoperator.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basics;
+
+public class Lab004_GradeCalculator_ternaryoperator {
+    public static void main(String[] args) {
+        int score= 78;
+       String grade= (score<100 && score>90)?"A":(score>80)?"B":(score>70)?"C":(score>60)?"D":"E";
+        System.out.println(grade);
+    }
+
+}
+//Another way--->
+// String grade = (score>90 && score<100)?"A":(score>80 && score<90)?"B":(score>70 && score<80)?"C":(score>60 && score<70)?"D":"E";
