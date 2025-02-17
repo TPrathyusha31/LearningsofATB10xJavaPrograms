16c9fc298b9f3a0ebbe3acc8ff7375c6013bb860
Prathyusha <tprathyushareddy31@gmail.com>
2025-02-17 23:34:44 +0530

    Feb 17th commit changes

---
+++ src/main/java/ex_15_Constructors/Lab071_default_constructor.java
@@ -0,0 +1,8 @@
+package ex_15_Constructors;
+
+public class Lab071_default_constructor {
+    public static void main(String[] args) {
+    Car car = new Car();
+        System.out.println(car.year_of_launch);
+    }
+}
