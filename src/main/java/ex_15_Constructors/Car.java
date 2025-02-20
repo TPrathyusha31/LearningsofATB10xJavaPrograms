97c9a6e145bcb078ad202fb9234669b0c3cfd74c
Prathyusha <tprathyushareddy31@gmail.com>
2025-02-20 23:13:47 +0530

    Feb 20th commit changes

---
+++ src/main/java/ex_15_Constructors/Car.java
@@ -0,0 +1,9 @@
+package ex_15_Constructors;
+
+public class Car{
+    int year_of_launch = 1990;
+    String name = "Maruthi";
+    Car(){
+        System.out.println("default constructor ");
+    }
+        }
