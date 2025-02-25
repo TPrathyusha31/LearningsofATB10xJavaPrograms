f8435fd1784b8b3a0658434cff0f77d706254880
Prathyusha <tprathyushareddy31@gmail.com>
2025-02-25 23:22:20 +0530

    Feb 25th commit changes

--- src/main/java/ex_15_Constructors/Lab074_CallingConstructorwithin_otherConstructor.java
+++ src/main/java/ex_15_Constructors/Lab074_CallingConstructorwithin_otherConstructor.java
@@ -1,5 +1,21 @@
 package ex_15_Constructors;
 
 public class Lab074_CallingConstructorwithin_otherConstructor {
+    public static void main(String[] args) {
+     Bike b = new Bike("Yamaha");
+    }
+}
+class Vehicle{
+    public Vehicle(){
+        System.out.println("This is a parent constructor - Vehicle");
+    }
+}
 
+class Bike extends Vehicle{
+    String model;
+    public Bike(String model){
+        super(); //When calling parent constructor in child constructor, the first statement in child constructor should be calling the parent using super () keyword
+        this.model=model;
+        System.out.println("The model of bike is : "+ model);
+    }
 }
